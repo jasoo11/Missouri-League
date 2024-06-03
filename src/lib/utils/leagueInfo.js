f68f4b58ba82097a453f71141ef96da9a1f6dcb9
jasoo11 <mr.mccartney3@gmail.com>
2024-06-03 14:08:08 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to the home of the Missouri League. </p>
+  <p> Welcome to the home of the Missouri League. </p>
 `;
 
 /*   STEP 3   */
